9ca4498128ed42f8d1cd154a43d8b15a021bb5a0
Will Garbutt <wgarbutt@gmail.com>
2023-06-09 19:32:29 +1200

    Added the ability to guess a whole word!

--- Wills_Words/game.py
+++ Wills_Words/game.py
@@ -2,14 +2,14 @@ import random
 
 
 def setup_game():
-    with open('words.txt', 'r') as wl:
+    with open('word_list.txt', 'r') as wl:
         words = wl.readlines()
         global correct_count
         correct_count = 0
         list_length = len(words)
         num = random.randint(0,list_length - 1)
         word = words[num].strip()
-        board = ["_", "_", "_", "_", "_"]
+        board = ["_" for i in range(len(word))]
         used_letters = []
         print("#################################\n#" '\x1b[6;30;42m' + " Welcome to Will's wordfinder! " + '\x1b[0m' + "#\n#################################")
         print("\nTry and guess this word!")
@@ -21,25 +21,27 @@ word, board, used_letters  = setup_game()
 while True:
     correct_count = board.count('_')
     while correct_count > 0:
-        #print(word)
         print(" ".join(board))
         print("\nGuesses: " + " ".join(used_letters))
-        user_input = input("Enter a new letter: ")
-        user_input = user_input.lower()
-        if len(user_input) == 1 and user_input.isalpha():
-            if user_input in word and user_input not in used_letters:
-                index_location = []
+        user_input = input("Enter your guess: ")
+        if len(user_input) == len(word):
+            if user_input == word:
+                board = list(word)
+                break
+            else:
+                print('\x1b[6;30;41m' + " Nope! That isnt the word we are looking for, try again! " + '\x1b[0m' )
+        elif len(user_input) == 1 and user_input.isalpha():
+            if user_input in used_letters:
+                print("You already guessed that letter. Try again.")
+            elif user_input in word:
                 for i in range(len(word)):
                     if word[i] == user_input:
-                        index_location.append(i)
-                for index in index_location:
-                    board[index] = user_input
-                correct_count += len(index_location)
+                        board[i] = user_input
                 used_letters.append(user_input)
-            elif user_input not in used_letters:
+            else:
                 used_letters.append(user_input)
         else:
-            print('Enter a single letter to continue.')
+            print("Invalid input. Please enter a valid letter or the entire word.")
             continue   
         correct_count = board.count('_')
     print("Congratulations " + '\x1b[6;30;42m' + word + '\x1b[0m' + " was the correct word!")
